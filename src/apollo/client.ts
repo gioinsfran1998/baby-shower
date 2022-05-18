a885d614932fbb525442063006e0bb41e190507e
gioinsfran1998 <gioinsfran98@gmail.com>
2022-05-18 08:38:24 -0400

    refactor: add log

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const URI = process.env.NEXT_PUBLIC_URI_API;
+console.log('URI', URI);
 
 const client = new ApolloClient({
   uri: URI || 'http://localhost:3002/api/graphql',
